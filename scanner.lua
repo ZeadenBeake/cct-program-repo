b6faca1bc4cbfafa7b99119bbf142809a909bb72
ZeadenBeake <130873151+ZeadenBeake@users.noreply.github.com>
2025-11-19 14:07:03 -0600

    Update scanner.lua
    - Improved text rending
    - New: Lists all found ores in "x[count] [ore]" format.
    - Changed default filter from "ore" to "_ore"

--- scanner.lua
+++ scanner.lua
@@ -1,6 +1,3 @@
--- This is a scanner program that looks for ores around the computer.
--- This program is made for a pocket computer with a AP GeoScanner equpped.
-
 local geo = peripheral.find("geoScanner")
 local pretty = require "cc.pretty"
 
@@ -9,30 +6,41 @@ geo.setFuelConsumptionRate(4)
 local args = { ... }
 
 while true do
+    local foundList = {}
     local closest = 0
     local closestDist = math.huge
     local found = 0
     local scan = geo.scan(8)
+    
+    term.clear()
+    term.setCursorPos(1, 1)
     for i, block in ipairs(scan) do
-        local match = args[1] or "ore"
+        local match = args[1] or "_ore"
         if string.match(block.name, match) then
             found = found + 1
-            print("Found " .. block.name .. "!")
+            --print("Found " .. block.name .. "!")
             local dist = math.abs(block.x) + math.abs(block.y) + math.abs(block.z)
             if dist < closestDist then
                 closest = i
                 closestDist = dist
             end
+            if not foundList[block.name] then
+                foundList[block.name] = 1
+            else
+                foundList[block.name] = foundList[block.name] + 1
+            end
         end
     end
     if found > 0 then
-        print("Found " .. found .. " ores!")
-        print("Closest is " .. closestDist .. " away!")
+        for ore, count in pairs(foundList) do
+            io.write("x" .. count .. " " .. ore .. "\n")
+        end
+        --print("Found " .. found .. " ores!")
+        io.write("Closest is " .. closestDist .. " away!\n")
         local closeBlock = scan[closest]
-        print("It's at ", closeBlock.x, closeBlock.y, closeBlock.z)
+        io.write("It's at " .. closeBlock.x .. ", " .. closeBlock.y .. ", " .. closeBlock.z .. "\n")
     else
-        print("Nothing found...")
+        io.write("Nothing found...\n")
     end
-    print("Cooling down...")
     os.sleep((geo.getOperationCooldown("scanBlocks")+10) / 1000)
 end
